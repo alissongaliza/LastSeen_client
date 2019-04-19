9d9ebb5c5b4d2f50859c32bccd7f523690f03cf3
Alisson Galiza <alissongaliza@gmail.com>
2019-04-19 12:25:47 -0300

    New library 'Paper' and fixed rendering error

--- App.js
+++ App.js
@@ -2,12 +2,13 @@ import React from "react";
 import { View, Text } from "react-native";
 import { createStackNavigator, createAppContainer } from "react-navigation";
 import ApolloClient from "apollo-boost";
-import {ApolloProvider} from 'react-apollo'
+import { ApolloProvider } from 'react-apollo'
+import { Provider as PaperProvider } from 'react-native-paper';
 
 import Home from './screens/Home'
 
 const client = new ApolloClient({
-    uri: "https://48p1r2roz4.sse.codesandbox.io"
+    uri: "http://192.168.56.1:4000"
 });
 
 
@@ -24,13 +25,15 @@ let Nav = createAppContainer(AppNavigator);
 export default class App extends React.Component {
     render() {
         return (
-            <ApolloProvider client={client}>
-                <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-                    <Nav />
+            <PaperProvider>
+                <View style={{ flex: 1}}>
+                    <ApolloProvider client={client}>
+                        <Nav />
+                    </ApolloProvider>
                 </View>
-            </ApolloProvider>
+            </PaperProvider>
         )
-        
+
     }
 }
 
--- screens/Home.js
+++ screens/Home.js
@@ -1,34 +1,32 @@
 import React from "react";
 import { View, Text } from "react-native";
-// import { Query } from "react-apollo";
-// import gql from "graphql-tag";
+import { Query, graphql } from "react-apollo";
+import gql from "graphql-tag";
 
-
-export default class Home extends React.Component {
-    render() {
-        return (
-            <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-                {/* <Query query={gql`{
-                    rates(currency: "USD") {
-                        currency
-                        rate
-                      }
-                }`}>
-                    {({ loading, error, data }) => {
-                        if (loading) return <Text>Loading...</Text>;
-                        if (error) return <Text>Error :(</Text>;
-                            return data.rates.map(({ currency, rate }) => (
-                                <View key={currency}>
-                                  <Text>{currency}: {rate}</Text>
-                                </View>
-                              ));
-
-                    }}
-                </Query>*/}
-                <Text>Home Screen</Text>
-            </View>
-        )
-
+const query = gql`{
+    searchPopularMovies{
+        title
     }
+}`
+
+const Home = ({ data: { searchPopularMovies,loading,error}  }) => {
+// const Home = (props) => {
+    // console.log(props);
+    
+    if(loading)
+        return <Text>Loading...</Text>
+    if(error)
+        return <Text>Error...</Text>
+    return (
+        <View style={{ flex: 1 }}>
+            {
+                searchPopularMovies.map(({ title }) => <Text key={title}>{title}</Text>)
+            }
+        </View>
+    )
+
+
 
 }
+
+export default graphql(query)(Home)
